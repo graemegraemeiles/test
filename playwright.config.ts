24bb875f6f11d4b800bb74c0f048083d248bec1f
Your Name <your_email@example.com>
2025-08-04 21:10:52 +0530

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 600_000,
+  timeout: 1200_000,
   testDir: './src',
   testMatch: 'playwright-script.ts',
   fullyParallel: true,
