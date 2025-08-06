a408752aaa1df5605828ce07391c24fa28ba164c
Your Name <your_email@example.com>
2025-08-06 22:29:14 +0530

    commit

--- src/script.cjs
+++ src/script.cjs
@@ -1,19 +1,42 @@
 const { chromium } = require('playwright-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-const randomWait = () => Math.floor(Math.random() * 30) * 1000;
+async function randomWait(page) {
+  const t = (15 + Math.floor(Math.random() * 30)) * 1000;
+  console.log(`Random wait for ${t / 1000} seconds `);
+  await page.waitForTimeout(t);
+}
 
 async function randomMouseMove(page) {
-  const x = Math.floor(Math.random() * 500);
-  const y = Math.floor(Math.random() * 500);
+  console.log('Perform random mouse move');
+  const x = Math.floor(Math.random() * 700);
+  const y = Math.floor(Math.random() * 700);
+  const sy = Math.floor(Math.random() * 700);
+
   await page.mouse.move(x, y);
-  await page.mouse.wheel(0, y);
+  await page.mouse.wheel(0, sy);
+}
+
+async function randomClick(page) {
+  console.log('Perform random click');
+
+  const x = Math.floor(Math.random() * 700);
+  const y = Math.floor(Math.random() * 700);
+  try {
+    await page.mouse.click(x, y);
+  } catch (e) {
+    console.error('Failed to click on new page:', e);
+  }
 }
 
 async function scenario2(page) {
   console.log('Running scenario 2');
   await randomMouseMove(page);
-  await page.waitForTimeout(randomWait());
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomWait(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
   await randomMouseMove(page);
 
   if (Math.random() < 0.3) {
@@ -27,7 +50,13 @@ async function scenario2(page) {
     }
   }
   await randomMouseMove(page);
-  await page.waitForTimeout(randomWait());
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomWait(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
   await randomMouseMove(page);
 
   if (true || Math.random() < 0.8) {
@@ -56,14 +85,25 @@ async function scenario2(page) {
     }
   }
   await randomMouseMove(page);
-  await page.waitForTimeout(randomWait());
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomWait(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
   await randomMouseMove(page);
 }
 
 async function scenario3(page) {
   console.log('Running scenario 3');
   await randomMouseMove(page);
-  await page.waitForTimeout(randomWait());
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomWait(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+
   if (Math.random() < 0.3) {
     try {
       console.log('Clicking next-btn');
@@ -74,7 +114,11 @@ async function scenario3(page) {
       console.error('Failed to click next-btn:', e);
     }
     await randomMouseMove(page);
-    await page.waitForTimeout(randomWait());
+    await randomMouseMove(page);
+    await randomMouseMove(page);
+    await randomWait(page);
+    await randomMouseMove(page);
+    await randomMouseMove(page);
     await randomMouseMove(page);
   }
 
@@ -97,7 +141,11 @@ async function scenario3(page) {
     throw e;
   }
   await randomMouseMove(page);
-  await page.waitForTimeout(randomWait());
+  await randomMouseMove(page);
+  await randomMouseMove(page);
+  await randomWait(page);
+  await randomMouseMove(page);
+  await randomMouseMove(page);
   await randomMouseMove(page);
 
   if (true || Math.random() < 0.8) {
@@ -125,7 +173,12 @@ async function scenario3(page) {
       console.error(`Failed to click ${randomVideoAd}:`, e);
     }
     await randomMouseMove(page);
-    await page.waitForTimeout(randomWait());
+    await randomMouseMove(page);
+    await randomMouseMove(page);
+
+    await randomWait(page);
+    await randomMouseMove(page);
+    await randomMouseMove(page);
     await randomMouseMove(page);
   }
 }
@@ -142,7 +195,7 @@ async function simulateUserInteraction(page) {
   await randomMouseMove(page);
   await randomMouseMove(page);
   const screenshot1 = await page.screenshot({
-    path: 'playwright-report/screenshot1.png',
+    path: 'screenshots/screenshot1.png',
     fullPage: true,
   });
   // testInfo.attach('screenshot1', {
@@ -159,7 +212,7 @@ async function simulateUserInteraction(page) {
   await randomMouseMove(page);
   await randomMouseMove(page);
   const screenshot2 = await page.screenshot({
-    path: 'playwright-report/screenshot2.png',
+    path: 'screenshots/screenshot2.png',
     fullPage: true,
   });
   // testInfo.attach('screenshot2', {
@@ -176,7 +229,7 @@ async function simulateUserInteraction(page) {
   await randomMouseMove(page);
   await randomMouseMove(page);
   const screenshot3 = await page.screenshot({
-    path: 'playwright-report/screenshot3.png',
+    path: 'screenshots/screenshot3.png',
     fullPage: true,
   });
   // testInfo.attach('screenshot3', {
@@ -193,7 +246,7 @@ async function simulateUserInteraction(page) {
   await randomMouseMove(page);
   await randomMouseMove(page);
   const screenshot4 = await page.screenshot({
-    path: 'playwright-report/screenshot4.png',
+    path: 'screenshots/screenshot4.png',
     fullPage: true,
   });
   // testInfo.attach('screenshot4', {
@@ -202,18 +255,123 @@ async function simulateUserInteraction(page) {
   // });
 }
 
-const stealth = StealthPlugin();
-chromium.use(stealth);
-chromium.launch({ headless: false }).then(async (browser) => {
-  const page1 = await browser.newPage();
+async function simualteOnAdPage(page) {
+  console.log('New page opened! Performing random mouse moves and clicks.');
+  // Perform a few random mouse moves and clicks
+  for (let i = 0; i < 3; i++) {
+    await randomMouseMove(page);
+    // Randomly click somewhere on the page
+    await randomMouseMove(page);
+    await randomClick(page);
 
-  console.log('Testing the stealth plugin..');
-  await page1.goto('https://bot.sannysoft.com', { waitUntil: 'networkidle' });
-  // await page1.waitForTimeout(10000000);
-  await page1.screenshot({ path: 'stealth.png', fullPage: true });
+    await randomMouseMove(page);
+    await randomClick(page);
 
-  console.log('All done, check the screenshot. ✨');
-  // await simulateUserInteraction(page1);
+    await randomMouseMove(page);
+    await randomClick(page);
+    await randomWait(page);
+    await randomMouseMove(page);
+    await randomClick(page);
 
-  await browser.close();
-});
+    await randomMouseMove(page);
+    await randomClick(page);
+  }
+}
+
+async function job() {
+  const stealth = StealthPlugin();
+  chromium.use(stealth);
+  chromium.launch({ headless: false }).then(async (browser) => {
+    // Listen for new pages (tabs) being opened
+    let ALL_ACTIONS = [];
+    ALL_ACTIONS.push(1);
+
+    const page1 = await browser.newPage();
+
+    const contexts = browser.contexts();
+    for (let i = 0; i < contexts.length; i++) {
+      console.log('Adding listners to context');
+
+      let context = contexts[i];
+      context.on('page', async (page) => {
+        ALL_ACTIONS.push(1);
+        await simualteOnAdPage(page);
+        ALL_ACTIONS.pop(1);
+        if (ALL_ACTIONS.length === 0) {
+          console.log('Closing browser page');
+
+          await browser.close();
+        }
+      });
+      context.on('popup', async (page) => {
+        ALL_ACTIONS.push(1);
+        await simualteOnAdPage(page);
+        ALL_ACTIONS.pop(1);
+        if (ALL_ACTIONS.length === 0) {
+          console.log('Closing browser popup');
+
+          await browser.close();
+        }
+      });
+    }
+
+    // console.log('Testing the stealth plugin..');
+    // await page1.goto('https://bot.sannysoft.com', { waitUntil: 'networkidle' });
+    // // await page1.waitForTimeout(10000000);
+    // await page1.screenshot({ path: 'stealth.png', fullPage: true });
+
+    // console.log('All done, check the screenshot. ✨');
+    await simulateUserInteraction(page1);
+    ALL_ACTIONS.pop(1);
+    if (ALL_ACTIONS.length === 0) {
+      console.log('Closing browser main');
+      await browser.close();
+    }
+
+    // await browser.close();
+  });
+}
+
+job();
+job();
+job();
+job();
+job();
+job();
+job();
+job();
+job();
+job();
+
+// chromium.launch({ headless: false }).then(async (browser) => {
+//   // Listen for new pages (tabs) being opened
+//   browser.on('page', async (page) => {
+//     console.log('New page opened! Performing random mouse moves and clicks.');
+//     // Perform a few random mouse moves and clicks
+//     for (let i = 0; i < 3; i++) {
+//       await randomMouseMove(page);
+//       // Randomly click somewhere on the page
+//       const x = Math.floor(Math.random() * 500);
+//       const y = Math.floor(Math.random() * 500);
+//       try {
+//         await page.mouse.click(x, y);
+//       } catch (e) {
+//         console.error('Failed to click on new page:', e);
+//       }
+//       await page.waitForTimeout(500 + Math.random() * 1000);
+//     }
+//   });
+//   const page1 = await browser.newPage();
+
+//   browser.addListener();
+
+//   // console.log('Testing the stealth plugin..');
+//   // await page1.goto('https://bot.sannysoft.com', { waitUntil: 'networkidle' });
+//   // // await page1.waitForTimeout(10000000);
+//   // await page1.screenshot({ path: 'stealth.png', fullPage: true });
+
+//   // console.log('All done, check the screenshot. ✨');
+//   await simulateUserInteraction(page1);
+
+//   await browser.close();
+// });
